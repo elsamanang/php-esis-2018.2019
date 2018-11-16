970a77deb9219c3067885b93c3b6c67276f5fe73
elsa.m <manangelsa07@gmail.com>
2018-11-16 07:28:01 +0100

    new

---
+++ ExSp/head.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Gestionnaire de taches</title>
+</head>
+<style>
+    h1{
+        text-align: center;
+    }
+    a{
+        text-decoration: none;
+    }
+    ul{
+        margin-left: 40%;
+    }
+    ul li, .b{
+        display: inline-block;
+        padding: 2% 3%;
+        border: solid 3px;
+        
+    }
+    ul li:hover, .b:hover{
+        background-color: rgba(0,100, 180, 0.6);
+        color: #fff;
+    }
+    body{
+        padding: 1% 8%;
+    }
+</style>
+<body>
+    <h1>Gestionnaire de taches</h1>
+    <hr>
+    <ul>
+        <li><a href="index.php">acceuil</a></li>
+        <li><a href="new.php">nouveau</a></li>
+    </ul>
