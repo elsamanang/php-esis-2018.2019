55563e8cde97de375eb0ae746b058fe66b827f1e
elsa.m <manangelsa07@gmail.com>
2018-11-16 07:57:54 +0100

    new

---
+++ expliqueSimplement/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>ExpliqueSimplement</title>
+</head>
+<body>
+    <h1>ExpliqueSimplement</h1>
+    <h3>Se connecter</h3>
+    <form method="post" action="connexion.php">
+        <label>Pseudo: </label>
+        <input type="text" name="pseudo" />
+        <br/>
+        <label>Mot de passe:</label>
+        <input type="password" name="mdp" />
+        <br/>
+        <input type="submit" value="Connexion" /> <a href="compte.php">Créer un nouveau compte</a>
+    </form>
+</body>
+</html>
